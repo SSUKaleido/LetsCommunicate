3c23c1cffe388eaf88515d14a7fc49c7374a1e9c
Kang Minsu <66078531+RtA0108@users.noreply.github.com>
2023-09-18 09:29:16 +0900

    동혁아 다 애니메이션 넣어서 바꿨다
    상점도 추가했다

--- Assets/Scripts/NightScripts/TurretDot.cs
+++ Assets/Scripts/NightScripts/TurretDot.cs
@@ -8,7 +8,7 @@ public class TurretDot : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private LayerMask enemyMask;
-    [SerializeField] private GameObject upgradeUI;
+    [SerializeField] private GameObject effect;
     [SerializeField] private Button upgradeButton;
     
     [Header("Attribute")]
@@ -37,7 +37,7 @@ public class TurretDot : MonoBehaviour
 
     private void FreezeEnemies(){
         RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)transform.position, 0f, enemyMask);
-
+        
         if(hits.Length > 0){
             for(int i =0; i < hits.Length; i++){
                 RaycastHit2D hit = hits[i];
@@ -46,6 +46,7 @@ public class TurretDot : MonoBehaviour
                 hl.TakeDamage(DotDamege);
             }
         }
+        
     }
 
 
